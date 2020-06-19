3faf7623bea8ae8dfb33c5cdd54e302ccde24165
xavier <xavierhoskins@outlook.com>
2020-06-19 11:29:20 -0500

    questions and count done

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,15 +1,14 @@
-#### Please add your answers to the ***Analysis of  Algorithms*** exercises here.
+#### Please add your answers to the **_Analysis of Algorithms_** exercises here.
 
 ## Exercise I
 
-a)
+a) O(n) because of while loop
 
+b) O(n^2) for loop and while loop combine the two
 
-b)
-
-
-c)
+c) still a O(n)
 
 ## Exercise II
 
-
+Start at floor 1 drop the egg if it breaks start over and if
+it doesnt break move on to the next one recursively.
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,16 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    # if the length of the word is less than 2 set to 0
+    if len(word) < 2:
+
+        return 0
+    # if the letters are equal to th then +1 and slice them off else keep slicing
+    elif word[:2] == "th":
+        
+         return count_th(word[2:]) + 1
+    else:
+        
+        return count_th(word[1:])
